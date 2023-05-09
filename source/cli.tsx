63b555ddf2ac34011869e7dece6e538686329eb4
Aljosja Jacobs <aljosja-online>
2023-05-09 23:01:47 +0200

    setup cli scaffolding

---
+++ source/cli.tsx
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import React from 'react';
+import {render} from 'ink';
+import meow from 'meow';
+import App from './app.js';
+
+const cli = meow(
+	`
+	Usage
+	  $ analytics-tools
+
+	Options
+		--name  Your name
+
+	Examples
+	  $ analytics-tools --name=Jane
+	  Hello, Jane
+`,
+	{
+		importMeta: import.meta,
+		flags: {
+			name: {
+				type: 'string',
+			},
+		},
+	},
+);
+
+render(<App name={cli.flags.name} />);
